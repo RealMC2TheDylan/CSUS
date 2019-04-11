e11bc8ecbcfcb13cc1dd72e07b4c2c48e7ebea1c
morganchilders <morganchilders2@gmail.com>
2019-04-11 23:20:36 +0000

    camerafeed.py edited online with Bitbucket

--- camerafeed.py
+++ camerafeed.py
@@ -2,7 +2,7 @@ import time
 import cv2
 import numpy as np
 from socket import *
-import json
+from StringIO import *
 
 
     
@@ -11,21 +11,28 @@ vc=cv2.VideoCapture(cameraID)
 thresh = 80
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_DGRAM )
+#clientSocket.connect((HOST, PORT))
 
 if vc.isOpened():
     rval, frame =vc.read()
 else:
     rval = False
     clientSocket.close()
-if rval:
+while rval:
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27:
-        print("we in key")
-    print("pre flatten",frame)    
-    frame=frame.flatten()
-    print("pre tostring", frame)
-    data = json.dumps({"vid": frame})
-    clientSocket.send(data.encode(),(HOST,PORT))    
-
+        break
+    
+   
+    frame= frame.flatten()
+    data = frame.tostring()
+    
+    clientSocket.sendall(data)
+    #clientSocket.sendto(data,(HOST,PORT))    
+#    while True: 
+ #       buffer_flag = clientSocket.recv(1024)
+  #      print(buffer_flag)
+#        if (buffer_flag):break
+clientSocket.close()
