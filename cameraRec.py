df85605d722b613b79738d05b3d2ed99bbc00b3d
morganchilders <morganchilders2@gmail.com>
2019-04-11 23:19:11 +0000

    cameraRec.py edited online with Bitbucket

--- cameraRec.py
+++ cameraRec.py
@@ -1,19 +1,23 @@
-import socket 
-
+from socket import *
+import numpy as np
+from StringIO import *
+import cv2
 UDP_IP = "127.0.0.1"
 UDP_PORT = 65432
+#cv2.namedWindow("preview")
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket(AF_INET, SOCK_DGRAM)
 
 sock.bind((UDP_IP, UDP_PORT))
+#cv2.namedWindow("preview", cv2.WINDOW_NORMAL)
+ultimate_buffer=''
+buffer_flag= 1
 
 while True:
-        data = sock.recvfrom(1024)
-        data = json.loads(data.decode())
-        frame = data.get("vid")
-        print(frame)
-
-
-
-
+    
+    data  = sock.recvfrom(57600)
+    frame = np.fromstring(data, dtype=np.uint8)
+    frame = np.reshape(frame,(120,160,3))
+    print(frame)
+    #cv2.imshow("preview",cv2.resize(frame,(640,480)))
+        
