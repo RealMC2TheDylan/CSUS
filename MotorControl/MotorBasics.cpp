4f0ba524d6b43fb37b62544b61d668259c049197
morganchilders <morganchilders2@gmail.com>
2019-04-18 03:28:43 +0000

    MotorBasics.cpp created online with Bitbucket

---
+++ MotorControl/MotorBasics.cpp
@@ -0,0 +1,60 @@
+#include <wiringSerial.h>
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int fd = serialOpen("/dev/ttySAC0", 38400);
+
+int constrain(int power, int low, int high){
+	if(power > high){
+		power = high;
+	}
+	else if(power < low){
+		power = low;
+	}
+	else
+	return power;
+}
+
+
+
+void motordrive(int motor, int power)
+{
+	char command;
+	char magnitude;
+	power = constrain(power, -63, 63);
+	if (motor == 1)
+	{
+		command = 64+power;
+	}
+	else if (motor == 2)
+	{
+		command = 191+power; // changed from 190 and 191
+	}
+
+	serialPuts(fd, &command);
+    serialPuts(fd, &command);
+}
+
+int main()
+{
+    int motor;
+    int p;
+    int i;
+
+    motordrive(1,0);
+
+    motordrive(2,0);
+	
+	for (p = 0; p < 63; p++)
+	{
+		motordrive(1, p);
+		motordrive(2, p);
+        delay(200);
+	}
+
+    motordrive(1,0);
+
+    motordrive(2,0);
+}
