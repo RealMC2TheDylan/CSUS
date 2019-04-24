1e689c399dcdc38f416b21f1ffa82e7ace2b1b1e
Andrew Lee (Wacky Wednesday) <nukefusion@me.com>
2019-04-24 19:53:47 +0000

    TCPCameraRecv.py edited online with Bitbucket

--- TCPCameraRecv.py
+++ TCPCameraRecv.py
@@ -6,7 +6,9 @@ import numpy as np
 import struct
 
 HOST = '127.0.0.1'
+#HOST = '192.168.0.100'
 PORT = 8083
+thresh = 68
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
@@ -36,5 +38,19 @@ while True:
 
     frame=pickle.loads(frame_data)
     print(frame.size)
-    cv2.imshow('frame', cv2.resize(frame,(640,480)))
-    cv2.waitKey(10)
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    frame_v = frame_hsv[:,:,2]
+    blurred = cv2.bilateralFilter(frame_v,9,150,150)
+    edges = cv2.Canny(blurred,thresh,thresh*2,L2gradient=True)
+    cv2.imshow("blur",cv2.resize(blurred,(640,480)))
+    cv2.imshow("edges",cv2.resize(edges,(640,480)))
+    _,mask = cv2.threshold(blurred,190,1,cv2.THRESH_BINARY)
+    erodeSize = 5
+    dilateSize = 7
+    eroded = cv2.erode(mask,np.ones((erodeSize,erodeSize)))
+    mask = cv2.dilate(eroded, np.ones((dilateSize, dilateSize)))
+    cv2.imshow("preview", cv2.resize(cv2.cvtColor(mask*edges,cv2.COLOR_GRAY2RGB) | frame, (640,480), interpolation = cv2.INTER_CUBIC))
+    #cv2.imshow('frame', cv2.resize(frame,(640,480)))
+    if cv2.waitKey(1) & 0xFF == ord ('q'):
+        break
+s.close()
