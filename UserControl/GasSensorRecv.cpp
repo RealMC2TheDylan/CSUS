2f7f7517861b0942c273974ebb89b27fbc6a66f6
Nicholas Nguyen <nicholasnguyen2@csus.edu>
2019-05-17 21:54:23 +0000

    GasSensorRecv.cpp created online with Bitbucket

---
+++ UserControl/GasSensorRecv.cpp
@@ -0,0 +1,43 @@
+//Windows gas sensor receiver requires MinGW to be installed for winsock2
+//compile code g++ -o gsr GasSensorRecv.cpp "C:\MinGW\lib\libws2_32.a"
+#include <iostream>
+#include <winsock2.h>
+#include <stdio.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    WSADATA WSAData;
+ 
+    SOCKET server, client;
+	int len;
+    SOCKADDR_IN serverAddr, clientAddr;
+ 
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+ 
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(10500);
+ 
+    bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+    listen(server, 0);
+ 
+    cout << "Listening for incoming connections..." << endl;
+	
+    char buf[256];
+    int clientAddrSize = sizeof(clientAddr);
+	while(1){
+			if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize))!= INVALID_SOCKET){
+					cout << "Client connected!" << endl;
+					while(recv(client,buf,sizeof(buf),0)){
+						//recv(client,buf,sizeof(buf),0);
+						printf("Gas Sensor Reads: %s\n",buf);
+						memset(buf,0,sizeof(buf));
+					}
+				}
+	}
+	closesocket(client);
+	cout << "Client DC." << endl;
+}
