3b5a1fddd8dab120730e5947736133d9e0e5c83d
Dylan McNeely <dylanmcneely.709@gmail.com>
2019-10-29 01:52:01 +0000

    UpdatingLabel.py created online with Bitbucket

---
+++ GUI/UpdatingLabel.py
@@ -0,0 +1,19 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import GLib
+i=0;
+class UpatingLabel(Gtk.Label):
+    def __init__(self):
+        Gtk.Label.__init__(self, "Welcome to UGV Command Center. Press connect to begin 0")
+        GLib.timeout_add_seconds(1, self.updateLabel)
+        self.updateLabel()
+    def updateLabel(self):
+        global i
+        i = self.updateNum(i)
+        timeStr = "Welcome to UGV Command Center. Press connect to begin " + str(i)
+        self.set_text(timeStr)
+        return GLib.SOURCE_CONTINUE
+    def updateNum(self, i):
+        i = i+1
+        return i
